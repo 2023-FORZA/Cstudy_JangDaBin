89d1b80b413975f90e19d431154c46bd8eb1ea30
Jangdb <t7s63q02@sookmyung.ac.kr>
2023-07-04 13:16:57 +0900

    1_Lab08

---
+++ 1_Lab08_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int N;
+	int count = 0;
+	int x[50], y[50];
+
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d %d", &x[i], &y[i]);
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		count = 0;
+
+		for (int j = 0; j < N; j++)
+		{
+			if (x[i] < x[j] && y[i] < y[j])
+				count++;
+		}
+		printf("%d ", count + 1);
+	}
+}
+
